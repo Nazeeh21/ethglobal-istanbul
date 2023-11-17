70568252929f017fa200b02b908bca981113c116
Dhaiwat <dhaiwatpandya@gmail.com>
2023-11-18 00:39:02 +0300

    Update web3modal-provider.tsx

--- frontend/components/web3modal-provider.tsx
+++ frontend/components/web3modal-provider.tsx
@@ -1,26 +1,23 @@
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react';
 
-import { WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet } from 'wagmi/chains'
+import { WagmiConfig } from 'wagmi';
+import { polygonMumbai } from 'wagmi/chains';
 
-const projectId = process.env.NEXT_PUBLIC_WC_ID || 'test-project-id'
+const projectId =
+  process.env.NEXT_PUBLIC_WC_ID || '95d7bf3fdce348e27d9d1e441d146b49';
 
 const metadata = {
   name: 'Web3Modal',
   description: 'Web3Modal Example',
   url: 'https://web3modal.com',
-  icons: ['https://avatars.githubusercontent.com/u/37784886']
-}
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+};
 
-const chains = [mainnet, arbitrum]
-const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+const chains = [polygonMumbai];
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
 
-createWeb3Modal({ wagmiConfig, projectId, chains })
+createWeb3Modal({ wagmiConfig, projectId, chains });
 
 export function Web3ModalProvider({ children }) {
-  return (
-    <WagmiConfig config={wagmiConfig}>
-        {children}
-    </WagmiConfig>
-  )
+  return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>;
 }
